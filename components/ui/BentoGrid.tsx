f14900c16c182bee229fab3d1a3e5bc697b4c2fa
Damidu.xd <damiduofc@gmail.com>
2024-09-17 20:56:23 +0530

    first commit

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "damiduofc@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
