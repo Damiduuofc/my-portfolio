ae87231a0ac1465def3a9ed593219d02445a491f
Damidu.xd <damiduofc@gmail.com>
2024-10-01 18:48:27 +0530

    projects updte

--- data/index.ts
+++ data/index.ts
@@ -51,8 +51,8 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a Saas App",
-    description: "story telling app",
+    title: "Currently building a Website",
+    description: "Responsive Hotel Booking Website using React JS ",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
